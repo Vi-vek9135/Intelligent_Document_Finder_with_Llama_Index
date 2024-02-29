7fd957813ce16e7773b3e1ab62db93e961616c1a
Vivek Roushan <it.20203007@gmail.com>
2024-02-29 14:49:09 +0530

    Download files from drive

--- To_Fetch_Multiples_documents_from_drive.py
+++ To_Fetch_Multiples_documents_from_drive.py
@@ -1,33 +1,59 @@
-import os
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-from dotenv import load_dotenv
-import openai
-from llama_index.llms.openai import OpenAI
+# import os
+# from pydrive.auth import GoogleAuth
+# from pydrive.drive import GoogleDrive
+# from dotenv import load_dotenv
+# import openai
+# from llama_index.llms.openai import OpenAI
 
-llm = OpenAI(temperature=0, model="gpt-3.5-turbo")
+# llm = OpenAI(temperature=0, model="gpt-3.5-turbo")
 
-gauth = GoogleAuth()
-drive = GoogleDrive(gauth)
-
-
-load_dotenv()  # Load the .env file
-
-openai.api_key = os.getenv('OPENAI_API_KEY')
-
-from llama_index.readers.google import GoogleDriveReader
+# gauth = GoogleAuth()
+# drive = GoogleDrive(gauth)
 
 
-loader = GoogleDriveReader()
+# load_dotenv()  # Load the .env file
 
-def load_data(folder_id: str):
-	docs = loader.load_data(folder_id=folder_id)
-	# for doc in docs:
-	# 	doc.id_ = doc.metadata["file_name"]
-		# doc.id_ = doc.metadata["bharat_text"]
-	return docs
+# openai.api_key = os.getenv('OPENAI_API_KEY')
+
+# from llama_index.readers.google import GoogleDriveReader
+
+
+# loader = GoogleDriveReader()
+
+# def load_data(folder_id: str):
+# 	docs = loader.load_data(folder_id=folder_id)
+# 	# for doc in docs:
+# 	# 	doc.id_ = doc.metadata["file_name"]
+# 		# doc.id_ = doc.metadata["bharat_text"]
+# 	return docs
 
 
 
 
 # This is the code to fetch multiple documents from the google drive by folder id.
+
+
+
+
+
+
+
+
+
+#This is for download files from google drive in local directory
+
+import os
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+gauth = GoogleAuth()
+drive = GoogleDrive(gauth)
+
+folder = '1uCxh7jmHBzU0ZUNix901qq2qkjkYHJPL'
+
+
+# Download files
+file_list = drive.ListFile({'q' : f"'{folder}' in parents and trashed=false"}).GetList()
+for index, file in enumerate(file_list):
+	print(index+1, 'file downloaded : ', file['title'])
+	file.GetContentFile(file['title'])
