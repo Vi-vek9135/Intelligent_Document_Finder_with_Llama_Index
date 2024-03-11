741408dac85b3ad6a99c4777c0c3c6c93d1bb5db
Vivek Roushan <76583731+Vi-vek9135@users.noreply.github.com>
2024-03-11 21:21:50 +0530

    Update schemas.py

--- User/schemas.py
+++ User/schemas.py
@@ -1,25 +1,39 @@
 from pydantic import BaseModel
 import datetime
 
+
+
+
+# User Creation Model
 class UserCreate(BaseModel):
     username: str
     email: str
     password: str
 
 
+
+# Request Details Model
 class requestdetails(BaseModel):
     email:str
     password:str
-        
+
+
+
+# Token Schema Model   
 class TokenSchema(BaseModel):
     access_token: str
     refresh_token: str
 
+
+# Change Password Model
 class changepassword(BaseModel):
     email:str
     old_password:str
     new_password:str
 
+
+
+# Token Creation Model
 class TokenCreate(BaseModel):
     user_id:str
     access_token:str
