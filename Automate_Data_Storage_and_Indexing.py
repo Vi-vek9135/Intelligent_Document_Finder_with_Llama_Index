22bcad21c3dc2f94a461c5d51adfb5e6f591aa5b
Vivek Roushan <76583731+Vi-vek9135@users.noreply.github.com>
2024-03-22 14:56:10 +0530

    Update Automate_Data_Storage_and_Indexing.py

--- Automate_Data_Storage_and_Indexing.py
+++ Automate_Data_Storage_and_Indexing.py
@@ -1,7 +1,6 @@
-# Importing necessary modules for data loading and indexing
-from To_Fetch_Multiples_documents_from_drive import load_data
+
 from llama_index.core import VectorStoreIndex
-from From_Drive_Link import returnfolder_id
+
 
 
 import streamlit as st
@@ -9,6 +8,8 @@ from llama_index.llms.openai import OpenAI
 from dotenv import load_dotenv
 import openai
 import os
+
+
 
 
 
@@ -39,40 +40,28 @@ llm = OpenAI(temperature=0, model="gpt-3.5-turbo")
 # Define a function to import documents by fetching them from Google Drive
 # The function is cached to avoid reloading and reindexing documents on every call
 @st.cache_resource(show_spinner=False)
-def import_docs_by_fetching_documents_from_drive(folder_id):
+def import_docs_by_fetching_documents_from_drive(documents):
 
     # Display a spinner in the Streamlit app to indicate that documents are being loaded and indexed
-    with st.spinner(text="Loading and indexing your docs – hang tight! This should take 1-2 minutes."):
+    with st.spinner(text="Indexing your docs – hang tight! This should take 2-3 minutes."):
 
-        # folder_id =  returnfolder_id()
-        # Load documents from the specified Google Drive folder ID
-        docs = load_data(folder_id=f"{folder_id}")
-        # docs = load_data(folder_id="1TcqWyNyxkjKmaLkSfl5yLBeB5IlJiGXa")
+      
+        index = VectorStoreIndex.from_documents(documents)
 
-        # Create a VectorStoreIndex from the loaded documents for efficient searching
-        index = VectorStoreIndex.from_documents(docs)
     return index
 
 
 
-# This is for automating the data storage and indexing.  
+
+def import_docs_by_fetching_documents_from_One_drive(documents):
+
+    # Display a spinner in the Streamlit app to indicate that documents are being loaded and indexed
+    with st.spinner(text="Indexing your docs – hang tight! This should take 2-3 minutes."):
+
+       
+        index = VectorStoreIndex.from_documents(documents)
+    return index
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# This is for automating the data loading. 
